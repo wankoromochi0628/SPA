c0f7b99e5b5e9be3aa368c8fb3221d049506bc6a
wankoromochi0628 <wankoromoti0628@gmail.com>
2019-11-14 00:05:39 +0900

    [add] vue 導入

---
+++ src/view/src/components/atoms/button.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="button is-success is-outlined" @click="$emit('regist')"></div>
+</template>
+
+<script>
+export default {
+    name: "btn"
+}
+</script>
---
+++ src/view/src/components/atoms/textbox.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="field">
+        <div class="control">
+            {{postSentence}}
+            <textarea v-model="sentence" class="textarea is-primary" placeholder=""></textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TextForm",
+        props: {
+            postSentence: {
+                type: String,
+                description: "入力項目"
+            }
+        },
+        computed: {
+            sentence: {
+                get() {
+                    return this.postSentence;
+                },
+                set(newSentence) {
+                    this.$emit('writtenSentence', newSentence);
+                }
+            }
+        }
+    };
+</script>
